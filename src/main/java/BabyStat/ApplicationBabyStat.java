9e2cf7e17b236e45ebc52bfbd629c882872875aa
Guzik27 <dguzikow27@gmail.com>
2020-04-11 20:35:07 +0200

    Initial commit

---
+++ src/main/java/BabyStat/ApplicationBabyStat.java
@@ -0,0 +1,11 @@
+package BabyStat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBabyStat {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationBabyStat.class, args);
+    }
+}
