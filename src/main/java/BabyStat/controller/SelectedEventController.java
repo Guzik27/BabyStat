4077ab58eac6326d8fcf9e7662c4a921dba2ead4
Guzik27 <dguzikow27@gmail.com>
2020-06-07 20:08:04 +0200

    Tworzy tabelę

---
+++ src/main/java/BabyStat/controller/SelectedEventController.java
@@ -0,0 +1,19 @@
+package BabyStat.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SelectedEventController {
+
+    @Value("${app.title.select}") // wstrzykiwanie do zmiennej title tego co mamy w application properties
+    private String title;
+
+    @GetMapping("select")
+    public String selectEventPage(Model model) {
+        model.addAttribute("title", title);
+        return "select";
+    }
+}
